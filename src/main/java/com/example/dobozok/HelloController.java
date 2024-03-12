7392aff8ae891118cb041805e0150cf9f7359a6c
Nundesz05 <nundesz06@gmail.com>
2024-03-12 12:59:33 +0100

    Timer

--- src/main/java/com/example/dobozok/HelloController.java
+++ src/main/java/com/example/dobozok/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.dobozok;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -10,13 +11,19 @@ public class HelloController {
     @FXML private Label closedDb;
     @FXML private Label openDb;
     @FXML private Pane Panel;
+    @FXML private ImageView Arrow;
 
     Label[][] lt= new Label[10][15];
     int[][] t = new int[10][15];
+
+    AnimationTimer timer = null;
+    int fok=0;
+
     @FXML
     protected void initialize() {
         for (int s=0;s<10;s++) {
             for (int o=0;o<15;o++) {
+                t[s][o]= 0;
                 int ss=s,oo=o;
                 lt[s][o]= new Label("");
                 lt[s][o].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("null.png"))));
@@ -27,6 +34,13 @@ public class HelloController {
                 Panel.getChildren().add(lt[s][o]);
             }
         }
-
+       timer= new AnimationTimer() {
+           @Override
+           public void handle(long now) {
+                   Arrow.setRotate(fok);
+                   fok++;
+               }
+       };
+    timer.start();
     }
 }
